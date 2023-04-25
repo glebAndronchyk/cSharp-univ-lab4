494e6b06f7817ea592747978a009ccf09a11dc82
rostikcheremys <rostikcheremys@gmail.com>
2023-04-25 20:57:18 +0300

    Adding a BlockThird

--- LB4/Rostik/Rostik.cs
+++ LB4/Rostik/Rostik.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using LB4.Components;
 using LB4.Structs;
 using LB4.utils;
@@ -73,12 +74,86 @@ namespace LB4.rostik
             Console.WriteLine("Результат:");
             _af.PrintArray(array);
         }
+        public void BlockThird()
+        {
+            List<List<int>> list = new List<List<int>>();
+            _af.Menu(list);
+            TaskThird(list);
+        }
+        private void TaskThird(List<List<int>> list)
+        {
+            int[][] arrayP = list.Select(l => l.ToArray()).ToArray();
+            int[][] arrayQ = new int[arrayP.Length][];
+            int maxColumns = arrayP[0].Length;
+            Random rand = new Random();
+
+            for (int i = 0; i < arrayP.Length; i++)
+            {
+                int lastZeroIndex = -1;
+
+                for (int j = arrayP[i].Length - 1; j >= 0; j--)
+                {
+                    if (arrayP[i][j] == 0)
+                    {
+                        lastZeroIndex = j;
+                        break;
+                    }
+                }
+
+                if (lastZeroIndex == -1)
+                {
+                    arrayQ[i] = new int[arrayP[i].Length];
+                }
+                else
+                {
+                    arrayQ[i] = new int[lastZeroIndex + 1];
+                }
+
+                for (int j = 0; j < arrayQ[i].Length; j++)
+                {
+                    arrayQ[i][j] = rand.Next(-100, 100);
+                }
+
+                for (int j = 1; j < arrayQ[i].Length; j++)
+                {
+                    int key = arrayQ[i][j];
+                    int k = j - 1;
+
+                    while (k >= 0 && arrayQ[i][k] < key)
+                    {
+                        arrayQ[i][k + 1] = arrayQ[i][k];
+                        k--;
+                    }
+                    arrayQ[i][k + 1] = key;
+                }
+
+                if (arrayQ[i].Length > maxColumns)
+                {
+                    maxColumns = arrayQ[i].Length;
+                }
+            }
+
+            int[] arrayZ = new int[arrayP.Length];
+
+            for (int i = 0; i < arrayP.Length; i++)
+            {
+                arrayZ[i] = arrayQ[i].Length;
+            }
+
+            Console.WriteLine("Матриця P:");
+            _af.PrintArray(arrayP);
+            Console.WriteLine("Масив Z:");
+            _af.PrintArray(arrayZ);
+            Console.WriteLine("Матриця Q:");
+            _af.PrintArray(arrayQ);
+        }
         public void InitTaskMenu()
         {
             Dictionary<int, MenuOptionStruct> menuOptions = new Dictionary<int, MenuOptionStruct>
             {
                 { 1, new MenuOptionStruct(BlockFirst) },
                 { 2, new MenuOptionStruct(BlockSecond) },
+                { 3, new MenuOptionStruct(BlockThird)}
             };
 
             MenuWithPreDefinedPlaceholder menu = new MenuFactory().CreateMenuWithPreDefinedPlaceholders(menuOptions);
